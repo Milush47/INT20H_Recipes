7003ba2b9c31eacb4eafebb8d3450204918505cf
NB1333 <91548015+NB1333@users.noreply.github.com>
2023-02-03 23:14:50 +0200

    Init project

---
+++ src/components/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <div class="profile">
+      <h2 class="title">Профіль</h2>
+      <div class="profile__info">
+        <div class="profile__info__text">
+          <h2 class="profile__info__text__name">Іванов Іван Іванович</h2>
+          <p class="profile__info__text__email"></p>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Profile"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/view/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <div class="wrapper">
+      <p>© 2021, Всі права захищені</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
