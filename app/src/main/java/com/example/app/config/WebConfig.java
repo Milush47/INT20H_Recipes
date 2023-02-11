c6a35b8013c97c8b05673c124120ba8102d92862
Milush47 <102389157+Milush47@users.noreply.github.com>
2023-02-12 00:59:11 +0100

    ADDED:  - allow headers cors

--- app/src/main/java/com/example/app/config/WebConfig.java
+++ app/src/main/java/com/example/app/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowedOrigins("http://localhost:5173")
                 .allowCredentials(true);
     }
