dd9eccd3c666ba3dd1ff359ae0bca3b2920cfc31
NBykoff <bykov.nikita@lll.kpi.ua>
2023-02-11 22:25:38 +0200

    ADDED: -filters

---
+++ src/components/view/Filters.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <div class="filter">
+      <h2 class="title">Фільтр</h2>
+      <div class="filter__info">
+        <div class="filter__info__text">
+          <h2 class="filter__info__text__name">Іванов Іван Іванович</h2>
+          <p class="filter__info__text__email"></p>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Filters"
+}
+</script>
+
+<style scoped>
+
+</style>
