3c51f21f9f5f608be19576f2faa4d28a1601e213
NBykoff <bykov.nikita@lll.kpi.ua>
2023-02-12 02:11:01 +0200

    EDITED: -file structure

--- src/service/productsService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export async function getProducts() {
-    const response = await axios.get('/my-products');
-    return response.data;
-}
--- src/service/recipesService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export async function getRecipes() {
-    const response = await axios.get('/recipes');
-    return response.data;
-}
