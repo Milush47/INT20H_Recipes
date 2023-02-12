4c92c0e6b91d0957e88558fef27133cde0fa3b61
NBykoff <bykov.nikita@lll.kpi.ua>
2023-02-12 02:08:31 +0200

    EDITED: -create recipes page -profile page -user service

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="wrapper">
-    <div class="profile">
-      <h2 class="title">Профіль</h2>
-      <div class="profile__info">
-        <div class="profile__info__text">
-          <h2 class="profile__info__text__name">Іванов Іван Іванович</h2>
-          <p class="profile__info__text__email"></p>
-          </div>
-      </div>
-    </div>
+  <div>
+    <h2>Profile</h2>
+    <p>First Name: {{ user.first_name }}</p>
+    <p>Last Name: {{ user.last_name }}</p>
+    <p>Email: {{ user.email }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: "Profile",
-  data: () => ({
-    user: "",
-    fields: [
-      { name: "firstname", value: "firstname" },
-      { name: "lastname", value: "lastname" },
-      { name: "email", value: "email" },
-    ],
-    link: "user",
-  })
-  }
+  name: 'Profile',
+  data() {
+    return {
+      user: {
+        first_name: '',
+        last_name: '',
+        email: '',
+      },
+    };
+  },
+  mounted() {
+    this.fetchUser();
+  },
+  methods: {
+    async fetchUser() {
+      try {
+        const res = await axios.get('http://localhost:8080/user');
+        this.user = res.data;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
