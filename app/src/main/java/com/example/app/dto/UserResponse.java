c9d6e0090c5a8be2df990ae6ea33accd7d32c7af
Milush47 <102389157+Milush47@users.noreply.github.com>
2023-03-06 17:02:31 +0100

    CHANGED:  - JSON properties UserResponse

--- app/src/main/java/com/example/app/dto/UserResponse.java
+++ app/src/main/java/com/example/app/dto/UserResponse.java
@@ -1,11 +1,16 @@
 package com.example.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 public record UserResponse(
+        @JsonProperty("firstname")
         String firstname,
+        @JsonProperty("lastname")
         String lastname,
+        @JsonProperty("email")
         String email,
+        @JsonProperty("preferences")
         String preferences
 
 ) {
