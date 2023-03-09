2e348cda9d5c818ed2a8e00cddfb9dfff9b34105
Milush47 <102389157+Milush47@users.noreply.github.com>
2023-03-09 01:10:25 +0100

    CHANGED:  - ResetPasswordRequest dto

--- app/src/main/java/com/example/app/dto/requests/ResetPasswordRequest.java
+++ app/src/main/java/com/example/app/dto/requests/ResetPasswordRequest.java
@@ -1,5 +1,6 @@
 package com.example.app.dto.requests;
 
+import com.example.app.validators.fieldMatch.FieldMatch;
 import com.example.app.validators.password.ValidPassword;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -7,11 +8,19 @@ import jakarta.validation.constraints.NotBlank;
 /*
     RegisterRequest is used for receiving data from resetPassword form
  */
+@FieldMatch.List(
+        @FieldMatch(
+                first   = "newPassword",
+                second  = "newPasswordConfirmation",
+                message = "pswds.must.match"
+        )
+)
 public record ResetPasswordRequest(
-        @Email(message = "email.valid")
-        @NotBlank(message = "email.is.req")
-        String email,
         @ValidPassword(message = "pswd.valid")
         @NotBlank(message = "pswd.is.req")
-        String newPassword
+        String newPassword,
+
+        @ValidPassword(message = "pswd.valid")
+        @NotBlank(message = "pswd.conf.is.req")
+        String newPasswordConfirmation
 ) {}
